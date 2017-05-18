462277dbc18c67a99c6f94a491276ddf7d730d33
Kristopher Merolla <kristopher.merolla@gmail.com>
2017-05-17 23:01:32 -0700

    Added link to database for register, password verification and hashing, and email check against database for existing entry

--- MySQL/assignments/login_and_registration/server.py
+++ MySQL/assignments/login_and_registration/server.py
@@ -1,13 +1,19 @@
 from flask import Flask, request, redirect, render_template, session, flash
 
-from mysqlconnection import MySQLConnector
+from mysqlconnection import MySQLConnector # needed to connect to SQL
+
+from validate_email import validate_email # import to check if email submitted is valid!
+
+import md5 # imports the md5 module to generate a hash for password securing
+
+import os, binascii # used for random generation // salt = binascii.b2a_hex(os.urandom(15))
 
 from validate_email import validate_email # import to check if email submitted is valid!
 
 app = Flask(__name__)
 mysql = MySQLConnector(app,'loginandregisterdb') #database name goes here!
 
-@app.route('/')
+@app.route('/') # login/register page
 def index():
 	return render_template('index.html')
 
@@ -15,11 +21,15 @@ def index():
 @app.route('/login', methods=['POST'])
 def login():
 	email = request.form['email']
-	password = request.form['password'] #password needs to be hashed here, use md5 and a salt
+	password = request.form['password'] # password needs to be hashed here, use md5 and a salt
 
-	#validate login credentials here
-	return redirect('/') #if invalid login credentials
-	return render_template('active.html') #if valid login credentials
+	# validate login credentials here
+
+	# if invalid login credentials
+	return redirect('/')
+
+	# if valid login credentials
+	return render_template('active.html')
 
 
 @app.route('/register')
@@ -32,12 +42,37 @@ def new_login():
 	first_name = request.form['first_name']
 	last_name = request.form['last_name']
 	email = request.form['email']
-	password = request.form['password'] #password needs to be hashed here, use md5 and a salt
+	#password needs to be hashed here, use md5 and a salt
+	password = request.form['password']
+	password_confirm = request.form['password_confirmation']
+	salt =  binascii.b2a_hex(os.urandom(15)) # salt is stored in the database for validation
+	hashed_pw = md5.new(password + salt).hexdigest() # this is stored in the database as the password, now secure
+	
+	#before inserting the user, validate entry
+	email_exists = "SELECT COUNT(*) FROM users WHERE email = :email"
+	exists_data = {'email': email}
+	if (mysql.query_db(email_exists,exists_data)[0]['COUNT(*)'] == 0): # if the email is NOT in the database...
+		if (validate_email(request.form['email'])):
+			if (password == password_confirm):
+				#build and execute the SQL insert for a new user creation
+				query_data = { 'first_name': first_name, 'last_name': last_name, 'email': email, 'hashed_pw': hashed_pw, 'salt': salt}
+				insert_query = "INSERT INTO users (first_name, last_name, email, password, salt, created_at, updated_at) VALUES (:first_name, :last_name, :email, :hashed_pw, :salt, NOW(), NOW())"
+				mysql.query_db(insert_query, query_data)
+			else:
+				# display error message password not matching password confirmation
+				print "ERROR< PASSWORDS DO NOT MATCH"
+				return redirect('/register')			
+		else:
+			# display error message if email is not a valid email
+			print "ERROR< EMAIL INVALID"
+			return redirect('/register')
+	else:
+		# display error message if email already exists
+		print "ERROR EMAIL EXISTS"
+		return redirect('/register')
 
-	#validate registration credentials here
-	return redirect('/') #if invalid registration credentials send back to login screen
-	return render_template('active.html') #if valid register credentials go to active page
-
+	# if all validations are met and the data is updated, redirect to the home page for login
+	return redirect('/')
 
 
 app.run(debug=True)
