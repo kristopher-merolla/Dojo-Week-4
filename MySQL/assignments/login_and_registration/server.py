1fa7cea5f5a652e36aa2807a872f1496e84c2b6e
Kristopher Merolla <kristopher.merolla@gmail.com>
2017-05-17 19:10:42 -0700

    Login and register assignment, basic setup completed, needs functionality and database actions

--- MySQL/assignments/login_and_registration/server.py
+++ MySQL/assignments/login_and_registration/server.py
@@ -12,8 +12,31 @@ def index():
 	return render_template('index.html')
 
 
+@app.route('/login', methods=['POST'])
+def login():
+	email = request.form['email']
+	password = request.form['password'] #password needs to be hashed here, use md5 and a salt
+
+	#validate login credentials here
+	return redirect('/') #if invalid login credentials
+	return render_template('active.html') #if valid login credentials
 
 
+@app.route('/register')
+def register():
+	return render_template('register.html')
+
+
+@app.route('/register_user', methods=['POST'])
+def new_login():
+	first_name = request.form['first_name']
+	last_name = request.form['last_name']
+	email = request.form['email']
+	password = request.form['password'] #password needs to be hashed here, use md5 and a salt
+
+	#validate registration credentials here
+	return redirect('/') #if invalid registration credentials send back to login screen
+	return render_template('active.html') #if valid register credentials go to active page
 
 
 
