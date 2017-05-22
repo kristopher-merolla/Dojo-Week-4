c6ca69d2919e64bb1eb304771c4a074944ac2c97
Kristopher Merolla <kristopher.merolla@gmail.com>
2017-05-22 10:23:53 -0700

    Adding wall and django environment

---
+++ djangoenv/djangoenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/AlienOutpost/Desktop/Coding_Dojo/Week_of_May_15_-_19/djangoenv/djangoenv/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
